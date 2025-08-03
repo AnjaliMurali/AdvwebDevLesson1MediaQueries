b8acf7739140ceffdc3790a0c2d537f8e4e0a6cf
Anjali <anjali.jetlearn@gmail.com>
2025-08-03 17:40:35 +0200

    Commit

---
+++ Mine - Responsive website/script.js
@@ -0,0 +1,11 @@
+window.onresize = screen;
+window.onload = screen;
+
+// Function named 'screen' 
+
+function screen() {
+  Width = window.innerWidth;
+  //height = window.innerHeight;
+  //document.getElementById("size").innerHTML = "Width : " + Width + " px" + " and Height : " + height + " px"
+  document.getElementById("size").innerHTML = "Width : " + Width + " px" 
+}
